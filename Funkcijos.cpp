36d5366eede9d6b1b5570e1ae709450df7ba772d
Hurabashi <minykasdo@gmail.com>
2020-10-16 11:16:23 +0300

    Add files via upload
    Created try catch statements for inputs. 
    Created functions and headers with diffrent files.
    Solved alerts occuring from floats and double comparing, by changing to floats.

---
+++ Funkcijos.cpp
@@ -0,0 +1,60 @@
+#include "Header_struct.h"
+
+void printer(string words) {
+    cout << words;
+}
+bool duomuo_sorter(duomuo const& lhs, duomuo const& rhs) {
+    if (lhs.Vard != rhs.Vard)
+        return lhs.Vard < rhs.Vard;
+    if (lhs.Pav != rhs.Pav)
+        return lhs.Pav < rhs.Pav;
+    return lhs.GP < rhs.GP;
+}
+
+bool isNumber(string s)
+{
+    for (int i = 0; i < s.length(); i++) { // pakeisti i strlen()
+        if (isdigit(s[i]) == false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int mustInputNumber(string message) {
+    string number;
+    while (true) {
+        try
+        {
+            cin >> number;
+            if (!isNumber(number)) {
+                throw "Ivedete ne skaiciu!!!\n";
+
+            }
+            break;
+        }
+        catch (const char* error) {
+            cout << error;
+            cout << message;
+        }
+    }
+
+    return stoi(number);
+}
+
+duomuo calculations(duomuo Eil) {
+    Eil.GP = Eil.GP / 5.0f;
+    Eil.GP = Eil.GP * 0.4f + 0.6f * Eil.egz;
+
+    std::sort(Eil.pazymiai.begin(), Eil.pazymiai.end());
+    if (Eil.pazymiai.size() % 2 == 1) {
+        Eil.GP_med = Eil.pazymiai[Eil.pazymiai.size() / 2];
+        Eil.GP_med = Eil.GP_med * 0.4f + 0.6f * Eil.egz;
+    }
+    else {
+        Eil.GP_med = (Eil.pazymiai[Eil.pazymiai.size() / 2] + Eil.pazymiai[(Eil.pazymiai.size() / 2) - 1]) / 2.0f;
+        Eil.GP_med = Eil.GP_med * 0.4f + 0.6f * Eil.egz;
+    }
+    return Eil;
+}
+
---
+++ Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+#include <cctype>
+
+using std::cout;
+using std::cin;
+using std::string;
